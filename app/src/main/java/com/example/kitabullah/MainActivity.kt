4c0f6fac9596ef20f4de05114831420c89760c3f
muhammad Luthfiyanto <luthfiyantomuhammad@gmail.com>
2023-01-31 09:37:56 +0700

    finish update main activity

--- app/src/main/java/com/example/kitabullah/MainActivity.kt
+++ app/src/main/java/com/example/kitabullah/MainActivity.kt
@@ -4,16 +4,49 @@ import android.os.Bundle
 import android.view.Menu
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.kitabullah.databinding.ActivityMainBinding
+import com.example.kitabullah.ui.FavoriteFragment.FavoriteFragment
+import com.example.kitabullah.ui.surah.SurahFragment
+import com.example.kitabullah.ui.tafsir.TafsirFragment
+import okhttp3.internal.platform.android.BouncyCastleSocketAdapter.Companion.factory
 
 class MainActivity : AppCompatActivity() {
 
+
+    private val mOnNavigationOnItemSelectedListener =
+        BottomNavigationView.OnNavigationItemSelectedListener { item ->
+            val fragment : Fragment?
+            when (item.itemId) {
+                R.id.navigation_surah -> {
+                    fragment = SurahFragment()
+                    loadFragment(fragment)
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.navigation_tafsir -> {
+                    fragment = TafsirFragment()
+                    loadFragment(fragment)
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.navigation_favorite -> {
+                    fragment = FavoriteFragment()
+                    loadFragment(fragment)
+                    return@OnNavigationItemSelectedListener true
+                }
+            }
+            false
+        }
+
+
     private var _binding: ActivityMainBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var viewModel: MainActivity
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,18 +54,16 @@ class MainActivity : AppCompatActivity() {
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navView: BottomNavigationView = binding.navView
+        binding.bttmNavigation.setOnNavigationItemSelectedListener(mOnNavigationOnItemSelectedListener)
 
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_surah, R.id.navigation_tafsir, R.id.navigation_favorite
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+    }
+
+
+    private fun loadFragment(fragment: Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.main_navigation, fragment)
+            .commit()
     }
 
     override fun onDestroy() {
